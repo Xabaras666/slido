ba4a0f132ff470ef3bb9ccd1f0ce4755f4c24361
Xabaras666 <dariotokicjajce@gmail.com>
2023-01-04 00:07:38 +0100

    added favicon, creating lecture and delelete lecture with a bug

---
+++ public/javascripts/lecture.js
@@ -0,0 +1,12 @@
+const button = document.getElementById('delete');
+button.addEventListener('click', function(e) {
+    fetch(window.location.href + '/delete' , {method: 'POST'}).then((response) => {
+        if(response.ok) {
+            return;
+        }
+        throw new Error('Request failed.');
+    })
+        .catch((error) => {
+            console.log(error);
+        })
+});
--- routes/lecture.js
+++ routes/lecture.js
@@ -1,6 +1,40 @@
 var express = require('express');
 var router = express.Router();
 const { pool } = require ('../dbConfig');
+const passport = require('passport');
+
+
+router.get('/newlecture', (req, res, next) => {
+    res.render('newlecture')
+})
+
+router.post('/newlecture', (req, res, next) => {
+    let {title, description, ending_date} = req.body;
+    let errors = [];
+
+    if(!title || !description || !ending_date) {
+        errors.push({message: "Please enter all fields!"});
+    }
+    if(errors.length > 0) {
+        res.render('newlecture', { errors })
+    }
+    else {
+        let current_lecturer = req.user.lecturer_id;
+        let current_date = new Date();
+
+        pool.query(
+            `INSERT INTO lecture (lecturer_id, title, description, creation_date, ending_date)
+            VALUES ($1, $2, $3, $4, $5)`,
+            [current_lecturer, title, description, current_date, ending_date],
+            (err, result) => {
+                if(err) {
+                    throw err;
+                }
+                res.redirect('/user/dashboard')
+            }
+        )
+    }
+})
 
 router.get('/:lecture_id', (req, res, next) => {
     pool.query(
@@ -19,5 +53,19 @@ router.get('/:lecture_id', (req, res, next) => {
         creation_date: req.lecture.creation_date,
         ending_date: req.lecture.ending_date})
 })
+
+router.post('/:lecture_id/delete', (req, res, next) => {
+    pool.query(
+        `DELETE FROM lecture WHERE lecture_id = $1`,
+        [req.params.lecture_id],
+        (err, result) => {
+            if (err) {
+                throw err;
+            }
+            return res.redirect('/user/dashboard')
+        }
+    )
+})
+
 
 module.exports = router;
